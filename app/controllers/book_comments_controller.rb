a146763ea0c3536304e05784fdec11c80524f70f
laemdfk <kappa021618@icloud.com>
2022-05-10 09:47:12 +0000

    [Add]book_comments controller追加、ルーティング追加

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,21 @@
+class BookCommentsController < ApplicationController
+  def create
+    book = Book.find(params[:book_id])
+    comment = current_user.comments.new(book_comment_params)
+    comment.book_id = book.id
+    comment.save
+    redirect_to request.referer
+    # referer = 保存等が完了後に、遷移元へリダイレクトさせるための処理
+  end
+
+  def destroy
+    BookComment.find(params[:id])
+    redirect_to request.referer
+  end
+
+
+  private
+   def book_comment_params
+     params.require(:book_comment).permit(:comment)
+   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'book_comments/create'
+  get 'book_comments/destroy'
   root to: 'homes#top'
   get 'home/about'=>"homes#about",as: 'about'
 
   devise_for :users
 
 
-  resources :books
+  resources :books do
+    resources :book_comments,only[:create, :destroy]
+  end
+  
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
